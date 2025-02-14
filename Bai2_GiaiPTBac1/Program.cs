d0236047715957841fcdcf114e532133cfe6210d
SPC <SPC@35-LAB402>
2025-02-14 10:18:54 +0700

    Add project files.

---
+++ Bai2_GiaiPTBac1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.Design;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a, b;
+        double x;
+        Console.WriteLine("nhap so a ");
+        a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("nhap so b ");
+        b =Convert.ToInt32(Console.ReadLine());
+        if (a == 0)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Phuong trinbh vo so nghiem");
+            }
+            else
+            {
+                Console.WriteLine("Phuong trinh vo nghiem");
+            }
+
+        }
+        else
+        {
+            x = (double)-b / a;
+            Console.WriteLine("Phuong trinh co nghiem" + x);
+        }
+    }
+}
